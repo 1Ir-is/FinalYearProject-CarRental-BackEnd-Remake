924065736d166e467c47a15bf5a91fcd4cb5c146
1Ir-is <mhuynk1005@gmail.com>
2024-05-07 08:32:20 +0700

    another end

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -21,6 +21,7 @@ namespace CarRental_BE.Controllers
         {
             _userRepository = userRepository;
             _mailService = mailService;
+            _tokenService = tokenService;
         }
 
 
